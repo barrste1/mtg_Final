26fe5cf6a7b25a1cd741c2c0b86dc0c4aabd98b2
Robbie-kress <68262512+Robbie-kress@users.noreply.github.com>
2020-09-14 10:30:12 -0400

    Identity Commit

---
+++ Controllers/CardController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MagicTheGatheringFinal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicTheGatheringFinal.Controllers
+{
+    public class CardController : Controller
+    {
+        /*rivate readonly <MagicDbContext> _context;*/
+        
+        //public CardController(MagicDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> CardList(string cardName)
+        {
+            Cardobject cardItem = await ScryfallDAL.GetApiResponse<Cardobject>("cards", "search", "https://api.scryfall.com", cardName);
+
+            return View(cardItem);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery] string order)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.scryfall.com/cards/search?order=name");
+            var response = await client.GetAsync("cards.json");
+            var results = await response.Content.ReadAsAsync<Cardobject>();
+            return View(results);
+        }
+    }
+
+    
+}
--- Models/ScryfallDAL.cs
+++ Models/ScryfallDAL.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -43,6 +46,77 @@ namespace MagicTheGatheringFinal.Models
             //final interation of for loop leaves + on end of string, so returning a substring without the last +
             query = query.Substring(0, query.Length - 1);
             return query;
+        }
+
+        public static List<KeyValuePair<string, string>> BuildApiArguments(params string[] pairs)
+        {
+            if (pairs.Length % 2 == 1)
+            {
+                return null;
+            }
+
+            var ret = new List<KeyValuePair<string, string>>();
+
+            for (int i = 0; i < pairs.Length; i += 2)
+            {
+                ret.Add(new KeyValuePair<string, string>(pairs[i], pairs[i + 1]));
+            }
+
+            return ret;
+        }
+        public static async Task<List<T>> GetApiResponseList<T>(string controller, string action, string baseUrl, string name,
+    params KeyValuePair<string, string>[] options) where T : new()
+        {
+            string url = $"{baseUrl}/" +
+                         $"{controller}/" +
+                         $"{action}/" +
+                         $"{name}";
+
+            bool first = true;
+            foreach (KeyValuePair<string, string> argument in options)
+            {
+                url += first ? "?" : "&";
+                url += $"{argument.Key}={Uri.EscapeDataString(argument.Value)}";
+                first = false;
+            }
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException)
+            {
+                return null;
+            }
+
+            Stream s = response.GetResponseStream();
+            if (s == null)
+            {
+                return null;
+            }
+
+            StreamReader rd = new StreamReader(s);
+
+            string output = await rd.ReadToEndAsync();
+            List<T> ret;
+            try
+            {
+                ret = JsonConvert.DeserializeObject<List<T>>(output);
+            }
+            catch (JsonSerializationException)
+            {
+                ret = new List<T> { JsonConvert.DeserializeObject<T>(output) };
+            }
+
+            return ret;
+        }
+        public static async Task<T> GetApiResponse<T>(string controller, string action, string baseUrl, string SpeciesName)
+    where T : new()
+        {
+            return (await GetApiResponseList<T>(controller, action, baseUrl, SpeciesName,
+                (new List<KeyValuePair<string, string>>()).ToArray())).FirstOrDefault();
         }
 
     }
