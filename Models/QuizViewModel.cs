9150fab370ff50ad0de26ebd1b6308d022daf332
Curtis Godzisz <curtisgodzisz@gmail.com>
2020-09-14 18:00:02 -0400

    quiz table

---
+++ Models/QuizViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicTheGatheringFinal.Models
+{
+    public class QuizViewModel
+    {
+        public string ColorScore { get; set; }
+        public QuizTable QuizTable { get; set; }
+        public int Counter { get; set; }
+        public int Answer { get; set; }
+    }
+}
