eb4ca9ae1978dfa04a57e528d97846706ddede17
unknown <barrste1@gmail.com>
2020-09-16 17:29:18 -0400

    started commander data pull to db

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,11 +11,11 @@ namespace MagicTheGatheringFinal.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly MagicDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(MagicDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -26,6 +26,52 @@ namespace MagicTheGatheringFinal.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> AddCommanders()
+        {
+            ScryfallDAL scryfall = new ScryfallDAL();
+            CardSearchObject commanders = new CardSearchObject();
+            CardsTable cardTable = new CardsTable();
+            commanders = await scryfall.GetSearch("?q=is:commander");
+
+            while (commanders.has_more)
+            {
+                for(int i = 0; i < commanders.data.Length; i++)
+                {
+                cardTable.CardArtUrl = commanders.data[i].image_uris.normal;
+                cardTable.CardId = commanders.data[i].id;
+                cardTable.Cmc = commanders.data[i].cmc;
+                cardTable.ManaCost = commanders.data[i].mana_cost;
+                cardTable.Name = commanders.data[i].name;
+                cardTable.OracleText = commanders.data[i].oracle_text;
+                cardTable.TypeLine = commanders.data[i].type_line;
+                if (commanders.data[i].color_identity.Contains("B"))
+                {
+                    cardTable.Black = "B";
+                }
+                if (commanders.data[i].color_identity.Contains("U"))
+                {
+                    cardTable.Blue = "U";
+                }
+                if (commanders.data[i].color_identity.Contains("W"))
+                {
+                    cardTable.White = "W";
+                }
+                if (commanders.data[i].color_identity.Contains("G"))
+                {
+                    cardTable.Green = "G";
+                }
+                if (commanders.data[i].color_identity.Contains("R"))
+                {
+                    cardTable.Red = "R";
+                }
+                _context.CardsTable.Add(cardTable);
+                _context.SaveChanges();
+                }
+            }
+
+            return  View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
