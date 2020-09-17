e31b5e1df156f342111d124fc4ef27ef8f20e6a0
AMoutousis <anmoutousis@gmail.com>
2020-09-16 22:36:07 -0400

    added logic for assisted deck builder that iterates through single removal, multi removal, ramp, and draw. Added logic to inner join deck and cards table to display finished deck list to view

--- Controllers/AssistedDeckBuilder.cs
+++ Controllers/AssistedDeckBuilder.cs
@@ -33,8 +33,71 @@ namespace MagicTheGatheringFinal.Controllers
 
             return View(removal);
         }
-        public async Task<IActionResult> FindMultiRemoval()
+        public async Task<IActionResult> FindMultiRemoval(List<string> SelectedCard)
         {
+            CardsTable cardTable = new CardsTable();
+            DecksTable deckTable = new DecksTable();
+
+            foreach (string assistedCardId in SelectedCard)
+            {
+                var userId = FindUserId();
+                cardTable.Id = 0;
+
+                //resetting card table color identity for each future iteration of the foreach loop
+                cardTable.White = null;
+                cardTable.Blue = null;
+                cardTable.Black = null;
+                cardTable.Red = null;
+                cardTable.Green = null;
+
+                if (_context.CardsTable.Where(x => x.CardId == assistedCardId).FirstOrDefault() == null)
+                {
+                    Cardobject cardItem = await ScryfallDAL.GetApiResponse<Cardobject>("cards", assistedCardId, "https://api.scryfall.com/", "");
+                    cardTable.CardArtUrl = cardItem.image_uris.normal;
+                    cardTable.CardId = cardItem.id;
+                    cardTable.Cmc = cardItem.cmc;
+                    cardTable.ManaCost = cardItem.mana_cost;
+                    cardTable.Name = cardItem.name;
+                    cardTable.OracleText = cardItem.oracle_text;
+                    cardTable.TypeLine = cardItem.type_line;
+                    if (cardItem.color_identity.Contains("B"))
+                    {
+                        cardTable.Black = "B";
+                    }
+                    if (cardItem.color_identity.Contains("U"))
+                    {
+                        cardTable.Blue = "U";
+                    }
+                    if (cardItem.color_identity.Contains("W"))
+                    {
+                        cardTable.White = "W";
+                    }
+                    if (cardItem.color_identity.Contains("G"))
+                    {
+                        cardTable.Green = "G";
+                    }
+                    if (cardItem.color_identity.Contains("R"))
+                    {
+                        cardTable.Red = "R";
+                    }
+                    _context.CardsTable.Add(cardTable);
+                    _context.SaveChanges();
+                }
+                var idCollection = (from x in _context.CardsTable where assistedCardId == x.CardId select x.Id).FirstOrDefault();
+                deckTable.CardId = idCollection;
+
+                if (userId != null)
+                {
+                    deckTable.AspUserId = userId;
+                }
+
+                deckTable.Id = 0;
+
+                _context.DecksTable.Add(deckTable);
+                _context.SaveChanges();
+            }
+
+
             string identity = FindPlayerType();
             ScryfallDAL dl = new ScryfallDAL();
             CardSearchObject removal = await dl.GetSearch($"id:{identity.ToLower()}+o:\"destroy all\"+t:\"sorcery\"ort:\"instant\"");
@@ -42,23 +105,216 @@ namespace MagicTheGatheringFinal.Controllers
             return View(removal);
         }
 
-        public async Task<IActionResult> FindRamp()
+        public async Task<IActionResult> FindRamp(List<string> SelectedCard)
         {
+            CardsTable cardTable = new CardsTable();
+            DecksTable deckTable = new DecksTable();
+
+            foreach (string assistedCardId in SelectedCard)
+            {
+                var userId = FindUserId();
+                cardTable.Id = 0;
+
+                //resetting card table color identity for each future iteration of the foreach loop
+                cardTable.White = null;
+                cardTable.Blue = null;
+                cardTable.Black = null;
+                cardTable.Red = null;
+                cardTable.Green = null;
+
+                if (_context.CardsTable.Where(x => x.CardId == assistedCardId).FirstOrDefault() == null)
+                {
+                    Cardobject cardItem = await ScryfallDAL.GetApiResponse<Cardobject>("cards", assistedCardId, "https://api.scryfall.com/", "");
+                    cardTable.CardArtUrl = cardItem.image_uris.normal;
+                    cardTable.CardId = cardItem.id;
+                    cardTable.Cmc = cardItem.cmc;
+                    cardTable.ManaCost = cardItem.mana_cost;
+                    cardTable.Name = cardItem.name;
+                    cardTable.OracleText = cardItem.oracle_text;
+                    cardTable.TypeLine = cardItem.type_line;
+                    if (cardItem.color_identity.Contains("B"))
+                    {
+                        cardTable.Black = "B";
+                    }
+                    if (cardItem.color_identity.Contains("U"))
+                    {
+                        cardTable.Blue = "U";
+                    }
+                    if (cardItem.color_identity.Contains("W"))
+                    {
+                        cardTable.White = "W";
+                    }
+                    if (cardItem.color_identity.Contains("G"))
+                    {
+                        cardTable.Green = "G";
+                    }
+                    if (cardItem.color_identity.Contains("R"))
+                    {
+                        cardTable.Red = "R";
+                    }
+                    _context.CardsTable.Add(cardTable);
+                    _context.SaveChanges();
+                }
+                var idCollection = (from x in _context.CardsTable where assistedCardId == x.CardId select x.Id).FirstOrDefault();
+                deckTable.CardId = idCollection;
+
+                if (userId != null)
+                {
+                    deckTable.AspUserId = userId;
+                }
+
+                deckTable.Id = 0;
+
+                _context.DecksTable.Add(deckTable);
+                _context.SaveChanges();
+            }
+
             string identity = FindPlayerType();
             ScryfallDAL dl = new ScryfallDAL();
             CardSearchObject removal = await dl.GetSearch($"id:{identity.ToLower()}+produces:br+t:\"artifact\"");
 
-            return View("FindSingleRemoval", removal);
+            return View(removal);
         }
 
-        public async Task<IActionResult> FindDraw()
+        public async Task<IActionResult> FindDraw(List<string> SelectedCard)
         {
+            CardsTable cardTable = new CardsTable();
+            DecksTable deckTable = new DecksTable();
+
+            foreach (string assistedCardId in SelectedCard)
+            {
+                var userId = FindUserId();
+                cardTable.Id = 0;
+
+                //resetting card table color identity for each future iteration of the foreach loop
+                cardTable.White = null;
+                cardTable.Blue = null;
+                cardTable.Black = null;
+                cardTable.Red = null;
+                cardTable.Green = null;
+
+                if (_context.CardsTable.Where(x => x.CardId == assistedCardId).FirstOrDefault() == null)
+                {
+                    Cardobject cardItem = await ScryfallDAL.GetApiResponse<Cardobject>("cards", assistedCardId, "https://api.scryfall.com/", "");
+                    cardTable.CardArtUrl = cardItem.image_uris.normal;
+                    cardTable.CardId = cardItem.id;
+                    cardTable.Cmc = cardItem.cmc;
+                    cardTable.ManaCost = cardItem.mana_cost;
+                    cardTable.Name = cardItem.name;
+                    cardTable.OracleText = cardItem.oracle_text;
+                    cardTable.TypeLine = cardItem.type_line;
+                    if (cardItem.color_identity.Contains("B"))
+                    {
+                        cardTable.Black = "B";
+                    }
+                    if (cardItem.color_identity.Contains("U"))
+                    {
+                        cardTable.Blue = "U";
+                    }
+                    if (cardItem.color_identity.Contains("W"))
+                    {
+                        cardTable.White = "W";
+                    }
+                    if (cardItem.color_identity.Contains("G"))
+                    {
+                        cardTable.Green = "G";
+                    }
+                    if (cardItem.color_identity.Contains("R"))
+                    {
+                        cardTable.Red = "R";
+                    }
+                    _context.CardsTable.Add(cardTable);
+                    _context.SaveChanges();
+                }
+                var idCollection = (from x in _context.CardsTable where assistedCardId == x.CardId select x.Id).FirstOrDefault();
+                deckTable.CardId = idCollection;
+
+                if (userId != null)
+                {
+                    deckTable.AspUserId = userId;
+                }
+
+                deckTable.Id = 0;
+
+                _context.DecksTable.Add(deckTable);
+                _context.SaveChanges();
+            }
+
             string identity = FindPlayerType();
             ScryfallDAL dl = new ScryfallDAL();
             CardSearchObject removal = await dl.GetSearch($"id:{identity.ToLower()}+o:draw");
 
-            return View("FindSingleRemoval", removal);
+            return View(removal);
         }
+
+        public async Task<IActionResult> CompleteAssistedDeck(List<string> SelectedCard)
+        {
+            CardsTable cardTable = new CardsTable();
+            DecksTable deckTable = new DecksTable();
+
+            foreach (string assistedCardId in SelectedCard)
+            {
+                var userId = FindUserId();
+                cardTable.Id = 0;
+
+                //resetting card table color identity for each future iteration of the foreach loop
+                cardTable.White = null;
+                cardTable.Blue = null;
+                cardTable.Black = null;
+                cardTable.Red = null;
+                cardTable.Green = null;
+
+                if (_context.CardsTable.Where(x => x.CardId == assistedCardId).FirstOrDefault() == null)
+                {
+                    Cardobject cardItem = await ScryfallDAL.GetApiResponse<Cardobject>("cards", assistedCardId, "https://api.scryfall.com/", "");
+                    cardTable.CardArtUrl = cardItem.image_uris.normal;
+                    cardTable.CardId = cardItem.id;
+                    cardTable.Cmc = cardItem.cmc;
+                    cardTable.ManaCost = cardItem.mana_cost;
+                    cardTable.Name = cardItem.name;
+                    cardTable.OracleText = cardItem.oracle_text;
+                    cardTable.TypeLine = cardItem.type_line;
+                    if (cardItem.color_identity.Contains("B"))
+                    {
+                        cardTable.Black = "B";
+                    }
+                    if (cardItem.color_identity.Contains("U"))
+                    {
+                        cardTable.Blue = "U";
+                    }
+                    if (cardItem.color_identity.Contains("W"))
+                    {
+                        cardTable.White = "W";
+                    }
+                    if (cardItem.color_identity.Contains("G"))
+                    {
+                        cardTable.Green = "G";
+                    }
+                    if (cardItem.color_identity.Contains("R"))
+                    {
+                        cardTable.Red = "R";
+                    }
+                    _context.CardsTable.Add(cardTable);
+                    _context.SaveChanges();
+                }
+                var idCollection = (from x in _context.CardsTable where assistedCardId == x.CardId select x.Id).FirstOrDefault();
+                deckTable.CardId = idCollection;
+
+                if (userId != null)
+                {
+                    deckTable.AspUserId = userId;
+                }
+
+                deckTable.Id = 0;
+
+                _context.DecksTable.Add(deckTable);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("DeckList", "Card");
+
+        }
+
         public string FindPlayerType()
         {
             string userId = FindUserId();
@@ -101,6 +357,74 @@ namespace MagicTheGatheringFinal.Controllers
             _context.SaveChanges();
 
             return assistedDeckName;
+        }
+
+        public async Task<IActionResult> ProcessCards(List<string> SelectedCard)
+        {
+            CardsTable cardTable = new CardsTable();
+            DecksTable deckTable = new DecksTable();
+
+            foreach (string assistedCardId in SelectedCard)
+            {
+                var userId = FindUserId();
+                cardTable.Id = 0;
+
+                //resetting card table color identity for each future iteration of the foreach loop
+                cardTable.White = null;
+                cardTable.Blue = null;
+                cardTable.Black = null;
+                cardTable.Red = null;
+                cardTable.Green = null;
+
+                if (_context.CardsTable.Where(x => x.CardId == assistedCardId).FirstOrDefault() == null)
+                {
+                    Cardobject cardItem = await ScryfallDAL.GetApiResponse<Cardobject>("cards", assistedCardId, "https://api.scryfall.com/", "");
+                    cardTable.CardArtUrl = cardItem.image_uris.normal;
+                    cardTable.CardId = cardItem.id;
+                    cardTable.Cmc = cardItem.cmc;
+                    cardTable.ManaCost = cardItem.mana_cost;
+                    cardTable.Name = cardItem.name;
+                    cardTable.OracleText = cardItem.oracle_text;
+                    cardTable.TypeLine = cardItem.type_line;
+                    if (cardItem.color_identity.Contains("B"))
+                    {
+                        cardTable.Black = "B";
+                    }
+                    if (cardItem.color_identity.Contains("U"))
+                    {
+                        cardTable.Blue = "U";
+                    }
+                    if (cardItem.color_identity.Contains("W"))
+                    {
+                        cardTable.White = "W";
+                    }
+                    if (cardItem.color_identity.Contains("G"))
+                    {
+                        cardTable.Green = "G";
+                    }
+                    if (cardItem.color_identity.Contains("R"))
+                    {
+                        cardTable.Red = "R";
+                    }
+                    _context.CardsTable.Add(cardTable);
+                    _context.SaveChanges();
+                }
+                var idCollection = (from x in _context.CardsTable where assistedCardId == x.CardId select x.Id).FirstOrDefault();
+                deckTable.CardId = idCollection;
+
+                if (userId != null)
+                {
+                    deckTable.AspUserId = userId;
+                }
+
+                deckTable.Id = 0;
+
+                _context.DecksTable.Add(deckTable);
+                _context.SaveChanges();
+            }
+
+
+            return RedirectToAction("DeckList");
         }
 
     }
---
+++ Models/AssistedDeckTaskListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicTheGatheringFinal.Models
+{
+    public class AssistedDeckTaskListViewModel
+    {
+        public bool addSingleRemoval { get; set; }
+        public bool addDraw { get; set; }
+        public bool addMultiRemoval { get; set; }
+        public bool addRamp { get; set; }
+        public bool addCreatures { get; set; }
+        public string deckName { get; set; }
+    }
+}
